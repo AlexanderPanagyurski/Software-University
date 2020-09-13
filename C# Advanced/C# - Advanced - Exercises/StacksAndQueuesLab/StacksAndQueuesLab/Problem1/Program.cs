78d831d394e95108bd18e64635084956126b853e
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-09-13 23:43:41 +0300

    Stacks

---
+++ C# Advanced/C# - Advanced - Exercises/StacksAndQueuesLab/StacksAndQueuesLab/Problem1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> reversedString = new Stack<char>(input);
+
+            while (reversedString.Count>0)
+            {
+                Console.Write(reversedString.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C# Advanced/C# - Advanced - Exercises/StacksAndQueuesLab/StacksAndQueuesLab/StacksAndQueuesAddExerc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StacksAndQueuesAddExerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
