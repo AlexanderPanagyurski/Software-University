a890f1404806e7987bbfaf2448a03854274d9be5
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-09-16 21:12:48 +0300

    Create MusicHub Db - Code First

---
+++ Entity Framework Core/Entity Framework Core - Exams/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        [ForeignKey(nameof(Producer))]
+        public int ProducedId { get; set; }
+        public Producer Producer { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; } = new HashSet<Song>();
+    }
+}
---
+++ Entity Framework Core/Entity Framework Core - Exams/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,44 @@
+﻿namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<SongPerformer>(entity =>
+            {
+                entity.HasKey(sp => new { sp.SongId, sp.PerformerId });
+
+                entity
+                    .HasOne(sp => sp.Song)
+                    .WithMany(s => s.SongPerformers)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity
+                    .HasOne(sp => sp.Performer)
+                    .WithMany(p => p.PerformerSongs)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+    }
+}
