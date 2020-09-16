091ec5c28a68e9a534b58e5abf8d4aec5644ce12
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-09-17 02:02:21 +0300

    Create ImportEmployee Dto

---
+++ Entity Framework Core/Entity Framework Core - Exams/C# DB Advanced Exam - 07 Dec 2019/TeisterMask/DataProcessor/ImportDto/ImportEmployeeDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using TeisterMask.Datasets;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    public class ImportEmployeeDto
+    {
+        [Required]
+        [MinLength(3), MaxLength(40)]
+        [RegularExpression(@"(^[A-Z]+$)|(^[a-z]+$)|(^[A-Z0-9]+$)|(^[a-z0-9]+$)")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9]{3}-[0-9]{3}-[0-9]{4}$")]
+        public string Phone { get; set; }
+
+        public ImportTaskIdDto[] Tasks { get; set; }
+    }
+}
---
+++ Entity Framework Core/Entity Framework Core - Exams/C# DB Advanced Exam - 07 Dec 2019/TeisterMask/Datasets/ImportTaskIdDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeisterMask.Datasets
+{
+    public class ImportTaskIdDto
+    {
+        public int Id { get; set; }
+    }
+}
