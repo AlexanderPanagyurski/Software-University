86c5af71d5781508021ad0cab20f7eafbad70498
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-09-24 01:37:19 +0300

    Create Route in SIS.MvcFramework

--- C# Web Basics/SIS/DemoApp/Program.cs
+++ C# Web Basics/SIS/DemoApp/Program.cs
@@ -1,5 +1,9 @@
 ﻿using SIS.HTTP;
+using SIS.HTTP.Enums;
+using SIS.MvcFramework;
 using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DemoApp
@@ -8,8 +12,42 @@ namespace DemoApp
     {
         static async Task Main(string[] args)
         {
+            var routeTable = new List<Route>();
+            routeTable.Add(new Route(HttpMethodType.GET, "/", Index));
+            routeTable.Add(new Route(HttpMethodType.GET, "/users/login", Login));
+            routeTable.Add(new Route(HttpMethodType.POST, "/users/login", DoLogin));
             var httpServer = new HttpServer(80);
-           await httpServer.StartAsync();
+            await httpServer.StartAsync();
+        }
+
+        public static HttpResponse Index(HttpRequest httpRequest)
+        {
+            var content = "<h1>Home Page</h1>";
+            byte[] stringContent = Encoding.UTF8.GetBytes(content);
+
+            var response = new HttpResponse(HttpResponseCodeType.OK, stringContent);
+
+            return response;
+        }
+
+        public static HttpResponse Login(HttpRequest httpRequest)
+        {
+            var content = "<h1>Login Page</h1>";
+            byte[] stringContent = Encoding.UTF8.GetBytes(content);
+
+            var response = new HttpResponse(HttpResponseCodeType.OK, stringContent);
+
+            return response;
+        }
+
+        public static HttpResponse DoLogin(HttpRequest httpRequest)
+        {
+            var content = "<h1>Login Page</h1>";
+            byte[] stringContent = Encoding.UTF8.GetBytes(content);
+
+            var response = new HttpResponse(HttpResponseCodeType.OK, stringContent);
+
+            return response;
         }
     }
 }
---
+++ C# Web Basics/SIS/SIS.MvcFramework/Route.cs
@@ -0,0 +1,23 @@
+﻿using SIS.HTTP;
+using SIS.HTTP.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.MvcFramework
+{
+    public class Route
+    {
+        public Route(HttpMethodType httpMethod, string path,Func<HttpRequest, HttpResponse> action)
+        {
+            this.Path = path;
+            this.HttpMethod = httpMethod;
+            this.Action = action;
+        }
+        public string Path { get; set; }
+
+        public HttpMethodType HttpMethod { get; set; }
+
+        public Func<HttpRequest, HttpResponse> Action { get; set; }
+    }
+}
