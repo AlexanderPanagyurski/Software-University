2751bef1b17a410535a54ba4cffbc66f73cb2612
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-09-24 17:36:24 +0300

    Create User Service Model

---
+++ Entity Framework Core/Entity Framework Core - Exercises/CarStore/CarStore/CarStore.Servcises/Models/User/UserServiceModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarStore.Services.Models.User
+{
+    public class UserServiceModel
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public DateTime RegisteredOn { get; set; }
+
+        public bool IsBanned { get; set; }
+
+        public DateTime? BannedOn { get; set; }
+    }
+}
