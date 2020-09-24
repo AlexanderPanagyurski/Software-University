f891682a06d13b6848dedaba435b63a1052b1948
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-09-24 17:32:40 +0300

    Create Comment Service Models

---
+++ Entity Framework Core/Entity Framework Core - Exercises/CarStore/CarStore/CarStore.Servcises/Models/Comments/CommentCreateServiceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarStore.Services.Models.Comments
+{
+    public class CommentCreateServiceModel
+    {
+        public string UserId { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime WrittenOn { get; set; }
+
+        public string CarId { get; set; }
+    }
+}
---
+++ Entity Framework Core/Entity Framework Core - Exercises/CarStore/CarStore/CarStore.Servcises/Models/Comments/CommentViewServiceModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarStore.Services.Models.Comments
+{
+    public class CommentViewServiceModel
+    {
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Text { get; set; }
+
+        public string WrittenOn { get; set; }
+    }
+}
