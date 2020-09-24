d100a1d2043a0cd175831c8322a4a68e6cdc24e0
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-09-24 17:41:21 +0300

    Create Country, State, City Service Models

---
+++ Entity Framework Core/Entity Framework Core - Exercises/CarStore/CarStore/CarStore.Servcises/Models/State/StateServiceModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarStore.Services.Models.State
+{
+    public class StateServiceModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
