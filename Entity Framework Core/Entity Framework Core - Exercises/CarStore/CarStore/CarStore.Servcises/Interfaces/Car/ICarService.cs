659dead6a29e3bef8231263de5d96dece1368054
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-09-24 18:01:17 +0300

    Create ICarService

---
+++ Entity Framework Core/Entity Framework Core - Exercises/CarStore/CarStore/CarStore.Servcises/Interfaces/Car/ICarService.cs
@@ -0,0 +1,84 @@
+﻿using CarStore.Services.Models.Cars;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarStore.Services.Interfaces.Car
+{
+    public interface ICarService
+    {
+        Task CreateAsync(CarCreateServiceModel model);
+
+        Task EditAsync(CarEditServiceModel model);
+
+        Task<IEnumerable<CarViewServiceModel>> GetByUserIdAsync(string userId, int page, int carPerPage, string orderBy);
+
+        Task<IEnumerable<CarViewServiceModel>> GetByEngineIdAsync(string engineId, int page, int carPerPage, string orderBy);
+
+        Task<IEnumerable<CarViewServiceModel>> GetBySearchAsync(string search, int page, int carPerPage, string orderBy);
+
+        Task<IEnumerable<CarViewServiceModel>> GetLatestAsync(int count, string userId);
+
+        Task<IEnumerable<CarViewServiceModel>> GetByCountryId(string countryId, int page, int carPerPage, string orderBy);
+
+        Task<IEnumerable<CarViewServiceModel>> GetByStateId(string stateId, int page, int carPerPage, string orderBy);
+
+        Task<IEnumerable<CarViewServiceModel>> GetByCityId(string cityId, int page, int carPerPage, string orderBy);
+
+
+
+        Task<int> GetCountByUserIdAsync(string userId);
+
+        Task<int> GetCountBySearchAsync(string search);
+
+        Task<int> GetCountByEngineIdAsync(string engineId);
+
+        Task<int> GetCounLatestAsync(string latest);
+
+        Task<int> GetCountByCountryIdAsync(string countryId);
+
+        Task<int> GetCountByStateIdAsync(string stateId);
+
+        Task<int> GetCountByCityIdAsync(string cityId);
+
+
+        bool Contains(string adId);
+
+        Task<CarViewServiceModel> GetByIdAsync(string id);
+
+        Task ArchiveByIdAsync(string id);
+
+        Task UnarchiveByIdAsync(string id);
+
+        Task PromoteByIdAsync(string id, int days);
+
+        Task UnpromoteByIdAsync(string id);
+
+        Task BanByIdAsync(string id);
+
+        Task UnbanByIdAsync(string id);
+
+        Task IncrementViewsAsync(string id);
+
+        Task<IEnumerable<CarViewServiceModel>> GetBannedCarsByUserIdAsync(string userId, int page);
+
+        Task<int> GetBannedCarsCountByUserIdAsync(string userId);
+
+        Task<IEnumerable<CarViewServiceModel>> GetArchivedCarsByUserIdAsync(string userId, int page);
+
+        Task<int> GetArchivedCarsCountByUserIdAsync(string userId);
+
+        Task<int> ArchiveAllExpireCarsAsync(DateTime expirationDate);
+
+        Task<int> UnPromoteAllExpiredCarAsync(DateTime expirationDate);
+
+        Task<int> GetAllCarsCountAsync();
+
+        Task<IEnumerable<CarViewByAdminViewModel>> GetAllCarsAsync(int page, int adsPerPage);
+
+        Task<Dictionary<string, int>> GetNewCarsCountByDaysFromThisWeekAsync();
+
+        Task<Dictionary<string, int>> GetCarsCountByCategoriesAsync();
+    }
+}
--- Entity Framework Core/Entity Framework Core - Exercises/CarStore/CarStore/CarStore.Servcises/Models/Cars/CarViewServiceModel.cs
+++ Entity Framework Core/Entity Framework Core - Exercises/CarStore/CarStore/CarStore.Servcises/Models/Cars/CarViewServiceModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarStore.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -13,7 +14,50 @@ namespace CarStore.Services.Models.Cars
         public string Model { get; set; }
 
         public decimal Price { get; set; }
+        public int Views { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? EditedOn { get; set; }
+
+        public bool IsArchived { get; set; }
+
+        public DateTime? ArchivedOn { get; set; }
 
 
+        public bool IsReported { get; set; }
+
+        public DateTime? ReportedOn { get; set; }
+
+        public bool IsBanned { get; set; }
+
+        public DateTime? BannedOn { get; set; }
+
+        public bool IsPromoted { get; set; }
+
+        public DateTime? PromotedOn { get; set; }
+
+        public DateTime? PromotedUntil { get; set; }
+
+        public double? FuelConsumption { get; set; }
+
+        public double? Mileage { get; set; }
+
+        public BodyType BodyType { get; set; }
+
+        public PreviousOwnersType? PreviousOwnersType { get; set; }
+        public DateTime? Registration { get; set; }
+
+        public GearType GearType { get; set; }
+
+        public string CityId { get; set; }
+
+        public string StateId { get; set; }
+ 
+        public string CountryId { get; set; }
+
+        public string EngineId { get; set; }
+
+        public string UserId { get; set; }
     }
 }
