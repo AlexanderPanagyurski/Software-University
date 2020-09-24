7a03527f5825691e13d821da3eef6e8e20eb0972
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-09-24 17:28:52 +0300

    Create Engine Service Model

---
+++ Entity Framework Core/Entity Framework Core - Exercises/CarStore/CarStore/CarStore.Servcises/Models/Engines/EngineServiceModel.cs
@@ -0,0 +1,20 @@
+﻿using CarStore.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarStore.Services.Models.Engines
+{
+    class EngineServiceModel
+    {
+        public string Id { get; set; }
+
+        public short HorsePower { get; set; }
+
+        public double EngineDisplacement { get; set; }
+
+        public FuelType FuelType { get; set; }
+
+        public EuroEmissionType? EuroEmissionType { get; set; }
+    }
+}
