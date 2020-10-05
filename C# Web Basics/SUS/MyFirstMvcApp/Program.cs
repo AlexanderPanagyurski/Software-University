b4df24a100f6bfde511713b52e3acd1c9fdc83b4
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-10-06 00:21:28 +0300

    Create Controller class

--- C# Web Basics/SUS/MyFirstMvcApp/Program.cs
+++ C# Web Basics/SUS/MyFirstMvcApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using SUS.HTTP;
+﻿using MyFirstMvcApp.Controllers;
+using SUS.HTTP;
 using System;
 using System.IO;
 using System.Linq;
@@ -13,7 +14,8 @@ namespace MyFirstMvcApp
         static async Task Main(string[] args)
         {
             IHttpServer server = new HttpServer();
-            server.AddRoute("/", HomePage);
+            HomeController homeController = new HomeController();
+            server.AddRoute("/", homeController.Index());
             server.AddRoute("/niki", (request) =>
             {
                 return new HttpResponse("text/html", new byte[] { 0x56, 0x57 });
@@ -24,36 +26,10 @@ namespace MyFirstMvcApp
             await server.StartAsync(80);
         }
 
-        static HttpResponse HomePage(HttpRequest request)
-        {
-            var responseHtml = "<h1>Welcome!</h1>" +
-                request.Headers.FirstOrDefault(x => x.Name == "User-Agent")?.Value;
-            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
-            var response = new HttpResponse("text/html", responseBodyBytes);
-            return response;
-        }
+      
 
-        static HttpResponse Favicon(HttpRequest request)
-        {
-            var fileBytes = File.ReadAllBytes("wwwroot/favicon.ico");
-            var response = new HttpResponse("image/vnd.microsoft.icon", fileBytes);
-            return response;
-        }
 
-        static HttpResponse About(HttpRequest request)
-        {
-            var responseHtml = "<h1>About...</h1>";
-            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
-            var response = new HttpResponse("text/html", responseBodyBytes);
-            return response;
-        }
-
-        static HttpResponse Login(HttpRequest request)
-        {
-            var responseHtml = "<h1>Login...</h1>";
-            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
-            var response = new HttpResponse("text/html", responseBodyBytes);
-            return response;
-        }
+      
+       
     }
 }
