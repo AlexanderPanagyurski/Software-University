67d32c47bcd6621192e148f2135a8bde5034534e
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-10-14 17:02:20 +0300

    Aply Migration for IdentityRole

---
+++ C# Web Basics/csharp-web-master/2020-Sept-Season/SUS/SUS.MvcFramework/Enums/IdentityRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SUS.MvcFramework
+{
+    public enum IdentityRole
+    {
+        Unknown = 0,
+        User = 1,
+        Admin = 2
+    }
+}
