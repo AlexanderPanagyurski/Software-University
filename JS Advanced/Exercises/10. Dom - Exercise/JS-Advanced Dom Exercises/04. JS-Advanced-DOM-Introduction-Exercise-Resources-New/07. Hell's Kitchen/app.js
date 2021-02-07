255b7cca351ac84f2c0292f22447fefec7d03bf3
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2021-02-07 17:54:05 +0200

    add html and css exercise

--- JS Advanced/Exercises/10. Dom - Exercise/JS-Advanced Dom Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ JS Advanced/Exercises/10. Dom - Exercise/JS-Advanced Dom Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,8 +1,37 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+    document.querySelector('#btnSend').addEventListener('click', onClick);
+    let bestRestaurant = {
+        name: '',
+        averageSalary: 0.00,
+        bestSalary: 0.00,
+    }
 
-   function onClick () {
-      //   TODO:
-      
-   }
+    function onClick() {
+        let input = document.getElementsByTagName('textarea')[0].value;
+        let restaurantsData = input.split(',');
+        for (let i = 0; i < restaurantsData.length; i++) {
+            let resutaurantName = restaurantsData[i].split(' - ')[0];
+            let employeesData = restaurantsData[i].split(' - ')[1].split(', ');
+            let currBestSalary = 0;
+            let salaries = 0;
+            let avgSalary = 0;
+            for (let j = 0; j < employeesData.length; j++) {
+                let employeeSalary = employeesData[j].split(' ')[1];
+                salaries += employeeSalary;
+                if (currBestSalary < employeeSalary) {
+                    currBestSalary = employeeSalary;
+                }
+            }
+            avgSalary = salaries / employeesData.length;
+            if (avgSalary > bestRestaurant.averageSalary) {
+                bestRestaurant.name = resutaurantName;
+                bestRestaurant.averageSalary = avgSalary;
+                bestRestaurant.bestSalary = currBestSalary;
+            }
+        }
+        document.getElementById('output').innerHTML = '<div id=\'bestRestaurant\'></div>';
+        document.getElementById('output').innerHTML = '<div id=\'workers\'></div>';
+        document.getElementById('bestRestaurant').innerHTML = '<h2>Best Restaurnat</h2>';
+        document.getElementById('bestRestaurant').innerHTML += `<p>${bestRestaurant.name}</p>`;
+    }
 }
