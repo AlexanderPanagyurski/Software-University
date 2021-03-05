8cd949b73aea830b2bf54ddcf077d30839d81841
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2021-03-05 19:44:15 +0200

    add commit

--- JS Advanced/Exercises/10. Dom - Exercise/JS-Advanced Dom Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
+++ JS Advanced/Exercises/10. Dom - Exercise/JS-Advanced Dom Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
@@ -1,4 +1,21 @@
 function solve() {
-
-    //TODO...
+    document.getElementById('result').value = '';
+    let fromNumber = document.getElementById('input').value;
+    let toNumber = document.getElementById('selectMenuTo').value;
+    if (toNumber === 'binary') {
+        let convertedNumber = '';
+        while (fromNumber !== 0) {
+            let reminder = fromNumber % 2;
+            convertedNumber += reminder;
+            fromNumber = Math.floor(fromNumber / 2);
+        }
+        convertedNumber + '';
+        convertedNumber = convertedNumber.split('').reverse().join('');
+        document.getElementById('result').value = convertedNumber;
+    } else if (toNumber === 'hexadecimal') {
+        let convertedNumber = '';
+        let hexString = parseInt(fromNumber).toString(16);
+        convertedNumber = hexString.toUpperCase();
+        document.getElementById('result').value = convertedNumber;
+    }
 }
