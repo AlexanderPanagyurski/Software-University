dd48d08e5863c9c7af51a1430ccf677f6cfdccb5
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2021-03-06 19:16:49 +0200

    extend time converter

--- JS Advanced/Exercises/11. JS-Advanced-DOM-Manipulations-Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/03. Time-Converter/solution.js
+++ JS Advanced/Exercises/11. JS-Advanced-DOM-Manipulations-Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/03. Time-Converter/solution.js
@@ -4,12 +4,37 @@ function attachEventsListeners() {
     let minutes = document.getElementById('minutes');
     let seconds = document.getElementById('seconds');
 
-    let event = document.getElementById('secondsBtn');
-    event.addEventListener('click', () => { convert(+seconds.value) });
+    let secondsEvent = document.getElementById('secondsBtn');
+    let daysEvent = document.getElementById('daysBtn');
+    let minutesEvent = document.getElementById('minutesBtn');
+    let hoursEvent = document.getElementById('hoursBtn');
 
-    function convert(sec) {
-        days.value = sec / (24 * 60 * 60);
-        hours.value = sec / (60 * 60);
-        minutes.value = sec / 60;
+    secondsEvent.addEventListener('click', () => { convertFromSeconds(+seconds.value) });
+    daysEvent.addEventListener('click', () => { convertFromDays(+days.value) });
+    minutesEvent.addEventListener('click', () => { convertFromMinutes(+minutes.value) });
+    hoursEvent.addEventListener('click', () => { convertFromHours(+hours.value) });
+
+    function convertFromSeconds(param) {
+        days.value = param / (24 * 60 * 60);
+        hours.value = param / (60 * 60);
+        minutes.value = param / 60;
+    }
+
+    function convertFromDays(param) {
+        hours.value = param * 24;
+        minutes.value = param * 24 * 60;
+        seconds.value = param * 24 * 60 * 60;
+    }
+
+    function convertFromMinutes(param) {
+        days.value = param * 24 * 60;
+        hours.value = param * 60;
+        seconds.value = param / 60;
+    }
+
+    function convertFromHours(param) {
+        days.value = param * 24;
+        minutes.value = param * 24 * 60;
+        seconds.value = param * 24 * 60 * 60;
     }
 }
