59af6b178f1370f95139ec46c05610e668463435
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2021-06-16 18:49:47 +0300

    Add QuickSort.js

---
+++ JS Advanced/Exercises/More Exercise/QuickSort.js
@@ -0,0 +1,42 @@
+    function partition(arr, low, high)
+    {
+        let temp;
+        let pivot = arr[high];
+   
+        // index of smaller element
+        let i = (low - 1);
+        for (let j = low; j <= high - 1; j++) {
+   
+            if (arr[j] <= pivot) {
+                i++;
+
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+   
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+   
+        return i + 1;
+    }
+   
+    function qSort(arr, low, high)
+    {
+        if (low < high) {
+            let pi = partition(arr, low, high);
+
+            qSort(arr, low, pi - 1);
+            qSort(arr, pi + 1, high);
+        }
+    }
+     
+    let n = 5;
+    let arr = [ 4, 2, 6, 9, 2 ];
+ 
+    qSort(arr, 0, n - 1);
+ 
+    for (let i = 0; i < n; i++)
+      document.write(arr[i] + " ");
